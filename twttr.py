885642c5472ca6d380d49c496db908b0ef074dae
saramostafa01 <saramostafafadlallah@gmail.com>
2024-10-07 23:36:10 +0300

    Update twttr.py

--- twttr.py
+++ twttr.py
@@ -1,5 +1,11 @@
-text = input("input text: ")
-for i in text:
-    if i.lower() == "e" or i.lower() == "a" or i.lower() == "i" or i.lower() == "o" or i.lower() == "u":
-        i = ""
-    print(i, end = "")
+def main():
+    text = input("input text: ")
+    vowels(text)
+
+def vowels(vow):
+    for i in vow:
+        if i.lower() == "e" or i.lower() == "a" or i.lower() == "i" or i.lower() == "o" or i.lower() == "u":
+            i = ""
+        print(i, end = "")
+
+main()
